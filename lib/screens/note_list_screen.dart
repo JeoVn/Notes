06a7f41e92eeb096355cd33afb14199bc02025f5
JeoVn <132755008+JeoVn@users.noreply.github.com>
2024-05-03 09:21:25 +0700

    note dialog

--- lib/screens/note_list_screen.dart
+++ lib/screens/note_list_screen.dart
@@ -1,6 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:notes/services/note_service.dart';
+import 'package:notes/widgets/note_dialog.dart';
 
 class NoteListScreen extends StatefulWidget {
   const NoteListScreen({super.key});
@@ -19,49 +21,59 @@ class _NoteListScreenState extends State<NoteListScreen> {
       appBar: AppBar(
         title: const Text('Notes'),
       ),
-      body: const NotesList(),
+      body: const NoteList(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showDialog(
             context: context,
             builder: (context) {
-              return AlertDialog(
-                content: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text('Add'),
-                    const Padding(
-                      padding: EdgeInsets.only(top: 10),
-                      child: Text('Title: ', textAlign: TextAlign.start),
-                    ),
-                    TextField(controller: _titleController),
-                    const Padding(
-                      padding: EdgeInsets.only(top: 10),
-                      child: Text('Description: ', textAlign: TextAlign.start),
-                    ),
-                    TextField(controller: _descriptionController),
-                  ],
-                ),
-                actions: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: const Text('Cancel'),
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      NoteService.addNote(_titleController.text,
-                              _descriptionController.text)
-                          .whenComplete(() => Navigator.of(context).pop());
-                    },
-                    child: const Text('Save'),
-                  ),
-                ],
-              );
+              return NoteDialog();
+              // return AlertDialog(
+              //   content: Column(
+              //     crossAxisAlignment: CrossAxisAlignment.start,
+              //     children: [
+              //       const Text('Add'),
+              //       const Padding(
+              //         padding: EdgeInsets.only(top: 10),
+              //         child: Text(
+              //           'Title: ',
+              //           textAlign: TextAlign.start,
+              //         ),
+              //       ),
+              //       TextField(
+              //         controller: _titleController,
+              //       ),
+              //       const Padding(
+              //         padding: EdgeInsets.only(top: 20),
+              //         child: Text(
+              //           'Description: ',
+              //         ),
+              //       ),
+              //       TextField(
+              //         controller: _descriptionController,
+              //       ),
+              //     ],
+              //   ),
+              //   actions: [
+              //     Padding(
+              //       padding: const EdgeInsets.symmetric(horizontal: 10),
+              //       child: ElevatedButton(
+              //         onPressed: () {
+              //           Navigator.of(context).pop();
+              //         },
+              //         child: const Text('Cancel'),
+              //       ),
+              //     ),
+              //     ElevatedButton(
+              //       onPressed: () {
+              //         NoteService.addNote(_titleController.text,
+              //                 _descriptionController.text)
+              //             .whenComplete(() => Navigator.of(context).pop());
+              //       },
+              //       child: const Text('Save'),
+              //     ),
+              //   ],
+              // );
             },
           );
         },
@@ -72,8 +84,8 @@ class _NoteListScreenState extends State<NoteListScreen> {
   }
 }
 
-class NotesList extends StatelessWidget {
-  const NotesList({super.key});
+class NoteList extends StatelessWidget {
+  const NoteList({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -91,88 +103,83 @@ class NotesList extends StatelessWidget {
           default:
             return ListView(
               padding: const EdgeInsets.only(bottom: 80),
-              children: snapshot.data!.map(
-                (document) {
-                  return Card(
-                    child: ListTile(
-                      onTap: () {
-                        showDialog(
-                          context: context,
-                          builder: (context) {
-                            TextEditingController titleController =
-                                TextEditingController(text: document['title']);
-                            TextEditingController descriptionController =
-                                TextEditingController(
-                                    text: document['description']);
-                            return AlertDialog(
-                              title: const Text('Update Data Notes'),
-                              content: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  const Text('Title: ', textAlign: TextAlign.start),
-                                  TextField(
-                                    controller: titleController,
-                                    // decoration: InputDecoration(
-                                    //   hintText: document['title'],
-                                    // ),
-                                  ),
-                                  const Padding(
-                                    padding: EdgeInsets.only(top: 10),
-                                    child: Text('Description: ',
-                                        textAlign: TextAlign.start),
-                                  ),
-                                  TextField(
-                                    controller: descriptionController,
-                                    // decoration: InputDecoration(
-                                    //   hintText: document['description'],
-                                    // ),
-                                  )
-                                ],
-                              ),
-                              actions: [
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 10),
-                                  child: ElevatedButton(
-                                    onPressed: () {
-                                      Navigator.of(context).pop();
-                                    },
-                                    child: const Text('Cancel'),
-                                  ),
-                                ),
-                                ElevatedButton(
-                                  onPressed: () {
-                                    NoteService.updateNote(
-                                            document['id'],
-                                            titleController.text,
-                                            descriptionController.text)
-                                        .whenComplete(
-                                            () => Navigator.of(context).pop());
-                                  },
-                                  child: const Text('update'),
-                                ),
-                              ],
-                            );
-                          },
-                        );
-                      },
-                      title: Text(document['title']),
-                      subtitle: Text(document['description']),
-                      trailing: InkWell(
-                        onTap: () {
-                          NoteService.deleteNote(document['id']);
-
+              children: snapshot.data!.map((document) {
+                return Card(
+                  child: ListTile(
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          // TextEditingController titleController =
+                          //     TextEditingController(text: document['title']);
+                          // TextEditingController descriptionController =
+                          //     TextEditingController(
+                          //         text: document['description']);
+                          return NoteDialog(note: document,);
+                          // return AlertDialog(
+                          //   title: const Text('Update Notes'),
+                          //   content: Column(
+                          //     crossAxisAlignment: CrossAxisAlignment.start,
+                          //     children: [
+                          //       const Text(
+                          //         'Title: ',
+                          //         textAlign: TextAlign.start,
+                          //       ),
+                          //       TextField(
+                          //         controller: titleController,
+                          //       ),
+                          //       const Padding(
+                          //         padding: EdgeInsets.only(top: 20),
+                          //         child: Text(
+                          //           'Description: ',
+                          //         ),
+                          //       ),
+                          //       TextField(
+                          //         controller: descriptionController,
+                          //       ),
+                          //     ],
+                          //   ),
+                          //   actions: [
+                          //     Padding(
+                          //       padding:
+                          //           const EdgeInsets.symmetric(horizontal: 10),
+                          //       child: ElevatedButton(
+                          //         onPressed: () {
+                          //           Navigator.of(context).pop();
+                          //         },
+                          //         child: const Text('Cancel'),
+                          //       ),
+                          //     ),
+                          //     ElevatedButton(
+                          //       onPressed: () {
+                          //         NoteService.updateNote(
+                          //                 document['id'],
+                          //                 titleController.text,
+                          //                 descriptionController.text)
+                          //             .whenComplete(
+                          //                 () => Navigator.of(context).pop());
+                          //       },
+                          //       child: const Text('Update'),
+                          //     ),
+                          //   ],
+                          // );
                         },
-                        child: const Padding(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 10),
-                          child: Icon(Icons.delete),
-                        ),
+                      );
+                    },
+                    title: Text(document['title']),
+                    subtitle: Text(document['description']),
+                    trailing: InkWell(
+                      onTap: () {
+                        NoteService.deleteNote(document['id']);
+                      },
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10),
+                        child: Icon(Icons.delete),
                       ),
                     ),
-                  );
-                },
-              ).toList(),
+                  ),
+                );
+              }).toList(),
             );
         }
       },
